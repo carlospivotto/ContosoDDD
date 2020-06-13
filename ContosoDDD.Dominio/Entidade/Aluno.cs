4b4c8578ce3ee47638db54f8a88a2f7d07cdfa33
Carlos Pivotto <pivotto@gmail.com>
2020-06-13 11:09:20 -0300

    Modelos de domínio.

---
+++ ContosoDDD.Dominio/Entidade/Aluno.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContosoDDD.Dominio.Entidade
+{
+    public class Aluno : BaseEntidade
+    {
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Email { get; set; }
+        public DateTime DataMatricula { get; set; }
+        public DateTime UltimoAcesso { get; set; }
+        public bool Ativo { get; }
+
+        public ICollection<Matricula> Matriculas { get; set; }
+
+        public bool AlunoAtivo()
+        {
+            TimeSpan intervalo = UltimoAcesso - DataMatricula;
+            return intervalo.Days <= 180;
+        }
+
+        public bool RegimeEspecial()
+        {
+            return DataMatricula.Year <= 2014;
+        }
+    }
+}
